9e93a34488400c72eb2430984fcec63fe6b8b1f2
OshriYoktan <oshriyok1@gmail.com>
2021-04-21 17:47:43 +0300

    mistertoys

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://oshri:<ff2891998ff>@cluster0.skaxd.mongodb.net/TOY_DB?retryWrites=true&w=majority',
+  'dbURL': 'mongodb+srv://oshri:ff2891998ff@cluster0.skaxd.mongodb.net/TOY_DB?retryWrites=true&w=majority',
 }
